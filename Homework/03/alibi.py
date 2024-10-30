017797cb007b08ce44ddae40f7ac5118c7b18d22
Vadim Sannikov <vadim.sannikov.a@gmail.com>
2024-10-30 22:44:15 +0300

    Added alibi

--- Homework/03/alibi.py
+++ Homework/03/alibi.py
@@ -1,3 +1,4 @@
+import math
 import torch
 
 
@@ -15,7 +16,21 @@ def compute_alibi(num_heads: int, seq_len: int) -> torch.Tensor:
     Returns:
         torch.Tensor: A tensor containing ALiBi to be added to attention scores.
     """
-    pass
+
+    # slopes = torch.pow(value, -torch.arange(0, num_heads).float() / num_heads)
+    # dist = torch.arange(seq_len).unsqueeze(0) - torch.arange(seq_len).unsqueeze(1)
+
+    alibi = torch.zeros((num_heads, seq_len, seq_len), dtype=torch.float32)
+    n = 2 ** math.floor(math.log2(num_heads))
+    value = 2.0 ** (8.0 / n)
+    if n < num_heads:
+        value = 2.0 ** (4.0 / n)
+    for k in range(num_heads):
+        for i in range(seq_len):
+            for j in range(seq_len):
+                alibi[k, i, j] = (j - i) / value ** (k + 1)
+
+    return alibi
 
 
 if __name__ == "__main__":
